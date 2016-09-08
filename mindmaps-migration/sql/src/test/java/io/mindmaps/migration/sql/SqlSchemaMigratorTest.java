a942716a854fee2b8d7c38f235ab323f1b1fd6dd
Alexandra Orth <alexandra@mindmaps.io>
2016-09-08 10:01:37 +0100

    renaming one file

--- mindmaps-migration/sql/src/test/java/io/mindmaps/migration/sql/SqlSchemaMigratorTest.java
+++ mindmaps-migration/sql/src/test/java/io/mindmaps/migration/sql/SqlSchemaMigratorTest.java
@@ -2,23 +2,23 @@ package io.mindmaps.migration.sql;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
-import io.mindmaps.MindmapsTransaction;
-import io.mindmaps.api.CommitLogController;
-import io.mindmaps.api.GraphFactoryController;
-import io.mindmaps.api.TransactionController;
+import io.mindmaps.MindmapsGraph;
 import io.mindmaps.core.Data;
-import io.mindmaps.core.MindmapsGraph;
 import io.mindmaps.core.implementation.exception.MindmapsValidationException;
 import io.mindmaps.core.model.RelationType;
 import io.mindmaps.core.model.ResourceType;
 import io.mindmaps.core.model.RoleType;
 import io.mindmaps.core.model.Type;
+import io.mindmaps.engine.controller.CommitLogController;
+import io.mindmaps.engine.controller.GraphFactoryController;
+import io.mindmaps.engine.controller.TransactionController;
+import io.mindmaps.engine.loader.BlockingLoader;
+import io.mindmaps.engine.util.ConfigProperties;
 import io.mindmaps.factory.GraphFactory;
-import io.mindmaps.loader.BlockingLoader;
-import io.mindmaps.util.ConfigProperties;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.sql.Connection;
@@ -33,12 +33,10 @@ public class SQLSchemaMigratorTest {
     private static final String GRAPH_NAME = "test";
 
     private MindmapsGraph graph;
-    private MindmapsTransaction transaction;
     private BlockingLoader loader;
 
     private Namer namer = new Namer() {};
     private static SQLSchemaMigrator migrator;
-    private Connection connection;
 
     @BeforeClass
     public static void start(){
@@ -58,24 +56,21 @@ public class SQLSchemaMigratorTest {
     @Before
     public void setup() throws MindmapsValidationException {
         graph = GraphFactory.getInstance().getGraphBatchLoading(GRAPH_NAME);
-        transaction = graph.getTransaction();
         loader = new BlockingLoader(GRAPH_NAME);
     }
 
     @After
     public void shutdown() throws SQLException {
         graph.clear();
-        graph.close();
         migrator.close();
-        connection.close();
     }
 
     @Test
     public void usersTest() throws SQLException {
-        connection = Util.setupExample("simple");
+        Connection connection = Util.setupExample("simple");
         migrator.configure(connection).migrate(loader);
 
-        Type type = transaction.getEntityType("USERS");
+        Type type = graph.getEntityType("USERS");
         assertNotNull(type);
 
         assertResourceRelationExists("ID", Data.LONG, type);
@@ -85,34 +80,34 @@ public class SQLSchemaMigratorTest {
 
     @Test
     public void alterTableTest() throws SQLException {
-        connection = Util.setupExample("alter-table");
+        Connection connection = Util.setupExample("alter-table");
         migrator.configure(connection).migrate(loader);
-        transaction.refresh();
+        graph.refresh();
 
-        Type cart = transaction.getEntityType("CART");
+        Type cart = graph.getEntityType("CART");
         assertNotNull(cart);
 
         assertResourceRelationExists("ID", Data.LONG, cart);
         assertResourceRelationExists("NAME", Data.STRING, cart);
 
-        Type cartItem = transaction.getEntityType("CART_ITEM");
+        Type cartItem = graph.getEntityType("CART_ITEM");
 
         assertResourceRelationExists("ITEM_QTY", Data.LONG, cartItem);
         assertResourceRelationExists("LAST_UPDATED", Data.STRING, cartItem);
 
-        Type category = transaction.getEntityType("CATEGORY");
+        Type category = graph.getEntityType("CATEGORY");
         assertResourceRelationExists("ID", Data.LONG, category);
         assertResourceRelationExists("DESCRIPTION", Data.STRING, category);
         assertResourceRelationExists("NAME", Data.STRING, category);
 
-        Type customer = transaction.getEntityType("CUSTOMER");
+        Type customer = graph.getEntityType("CUSTOMER");
         assertResourceRelationExists("ID", Data.LONG, customer);
         assertResourceRelationExists("NAME", Data.STRING, customer);
         assertResourceRelationExists("PASSWORD", Data.STRING, customer);
         assertResourceRelationExists("LAST_UPDATED", Data.STRING, customer);
         assertResourceRelationExists("REGISTRATION_DATE", Data.STRING, customer);
 
-        Type product = transaction.getEntityType("PRODUCT");
+        Type product = graph.getEntityType("PRODUCT");
 
         assertRelationExists(cart, customer, "CUSTOMER_ID");
         assertRelationExists(cartItem, cart, "CART_ID");
@@ -122,26 +117,26 @@ public class SQLSchemaMigratorTest {
 
     @Test
     public void emptyTest() throws SQLException {
-        connection = Util.setupExample("empty");
+        Connection connection = Util.setupExample("empty");
         migrator.configure(connection).migrate(loader);
-        transaction.refresh();
+        graph.refresh();
 
-        System.out.println(transaction.getMetaType().instances());
-        assertTrue(transaction.getMetaType().instances().size() == 8);
-        assertTrue(transaction.getMetaEntityType().instances().size() == 0);
-        assertTrue(transaction.getMetaRelationType().instances().size() == 0);
-        assertTrue(transaction.getMetaResourceType().instances().size() == 0);
-        assertTrue(transaction.getMetaRoleType().instances().size() == 0);
-        assertTrue(transaction.getMetaRuleType().instances().size() == 2);
+        System.out.println(graph.getMetaType().instances());
+        assertTrue(graph.getMetaType().instances().size() == 8);
+        assertTrue(graph.getMetaEntityType().instances().size() == 0);
+        assertTrue(graph.getMetaRelationType().instances().size() == 0);
+        assertTrue(graph.getMetaResourceType().instances().size() == 0);
+        assertTrue(graph.getMetaRoleType().instances().size() == 0);
+        assertTrue(graph.getMetaRuleType().instances().size() == 2);
     }
 
     @Test
     public void datavaultSchemaTest() throws SQLException {
-        connection = Util.setupExample("datavault");
+        Connection connection = Util.setupExample("datavault");
         migrator.configure(connection).migrate(loader);
-        transaction.refresh();
+        graph.refresh();
 
-        Type entity = transaction.getEntityType("AZ_BAKUAPPEALCOURT_CASES");
+        Type entity = graph.getEntityType("AZ_BAKUAPPEALCOURT_CASES");
         assertNotNull(entity);
         assertResourceRelationExists("ID", Data.LONG, entity);
         assertResourceRelationExists("DATE", Data.STRING, entity);
@@ -152,11 +147,11 @@ public class SQLSchemaMigratorTest {
 
     @Test
     public void postgresSchemaTest() throws SQLException, ClassNotFoundException {
-        connection = Util.setupExample("postgresql-example");
+        Connection connection = Util.setupExample("postgresql-example");
         migrator.configure(connection).migrate(loader);
-        transaction.refresh();
+        graph.refresh();
 
-        Type city = transaction.getEntityType("CITY");
+        Type city = graph.getEntityType("CITY");
         assertNotNull(city);
 
         assertResourceRelationExists("ID", Data.LONG, city);
@@ -165,28 +160,28 @@ public class SQLSchemaMigratorTest {
         assertResourceRelationExists("DISTRICT", Data.STRING, city);
         assertResourceRelationExists("POPULATION", Data.LONG, city);
 
-        Type country = transaction.getEntityType("COUNTRY");
+        Type country = graph.getEntityType("COUNTRY");
         assertNotNull(country);
 
-        Type language = transaction.getEntityType("COUNTRYLANGUAGE");
+        Type language = graph.getEntityType("COUNTRYLANGUAGE");
         assertNotNull(language);
 
         assertRelationExists(country, city, "CAPITAL");
         assertRelationExists(language, country, "COUNTRYCODE");
 
-        RoleType countryCodeChild = transaction.getRoleType("COUNTRYCODE-child");
+        RoleType countryCodeChild = graph.getRoleType("COUNTRYCODE-child");
         assertTrue(country.playsRoles().contains(countryCodeChild));
     }
 
     @Test
     public void mysqlSchemaTest() throws SQLException {
-        connection = Util.setupExample("mysql-example");
+        Connection connection = Util.setupExample("mysql-example");
         migrator.configure(connection).migrate(loader);
-        transaction.refresh();
+        graph.refresh();
 
-        System.out.println(transaction.getMetaEntityType().instances());
-        Type pet = transaction.getEntityType("PET");
-        Type event = transaction.getEntityType("EVENT");
+        System.out.println(graph.getMetaEntityType().instances());
+        Type pet = graph.getEntityType("PET");
+        Type event = graph.getEntityType("EVENT");
         assertNotNull(pet);
         assertNotNull(event);
 
@@ -205,11 +200,11 @@ public class SQLSchemaMigratorTest {
 
     @Test
     public void combinedKeyTest() throws SQLException {
-        connection = Util.setupExample("combined-key");
+        Connection connection = Util.setupExample("combined-key");
         migrator.configure(connection).migrate(loader);
-        transaction.refresh();
+        graph.refresh();
 
-        Type type = transaction.getEntityType("USERS");
+        Type type = graph.getEntityType("USERS");
         assertNotNull(type);
 
         assertResourceRelationExists("FIRSTNAME", Data.STRING, type);
@@ -218,7 +213,7 @@ public class SQLSchemaMigratorTest {
     }
 
     private ResourceType assertResourceExists(String name, Data datatype) {
-        ResourceType resourceType = transaction.getResourceType(name);
+        ResourceType resourceType = graph.getResourceType(name);
         assertNotNull(resourceType);
         assertEquals(datatype.getName(), resourceType.getDataType().getName());
         return resourceType;
@@ -228,9 +223,9 @@ public class SQLSchemaMigratorTest {
         String resourceName = namer.resourceName(owner.getId(), name);
         ResourceType resource = assertResourceExists(resourceName, datatype);
 
-        RelationType relationType = transaction.getRelationType("has-" + resourceName);
-        RoleType roleOwner = transaction.getRoleType("has-" + resourceName + "-owner");
-        RoleType roleOther = transaction.getRoleType("has-" + resourceName + "-value");
+        RelationType relationType = graph.getRelationType("has-" + resourceName);
+        RoleType roleOwner = graph.getRoleType("has-" + resourceName + "-owner");
+        RoleType roleOther = graph.getRoleType("has-" + resourceName + "-value");
 
         assertNotNull(relationType);
         assertNotNull(roleOwner);
@@ -245,9 +240,9 @@ public class SQLSchemaMigratorTest {
 
     private void assertRelationExists(Type owner, Type other, String relation) {
 
-        RelationType relationType = transaction.getRelationType(relation + "-relation");
-        RoleType roleOwner = transaction.getRoleType(relation + "-parent");
-        RoleType roleOther = transaction.getRoleType(relation + "-child");
+        RelationType relationType = graph.getRelationType(relation + "-relation");
+        RoleType roleOwner = graph.getRoleType(relation + "-parent");
+        RoleType roleOther = graph.getRoleType(relation + "-child");
 
         assertNotNull(relationType);
         assertNotNull(roleOwner);
