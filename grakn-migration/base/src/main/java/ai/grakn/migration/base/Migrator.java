a47edb3ae7ce0a3ed0c98399a18bb04a507b04ee
Alexandra Orth <alexandra@mindmaps.io>
2017-06-20 10:50:17 +0100

    Handle casting exception

--- grakn-migration/base/src/main/java/ai/grakn/migration/base/Migrator.java
+++ grakn-migration/base/src/main/java/ai/grakn/migration/base/Migrator.java
@@ -22,6 +22,7 @@ import ai.grakn.client.BatchMutatorClient;
 import ai.grakn.exception.GraqlSyntaxException;
 import ai.grakn.graql.Graql;
 import ai.grakn.graql.Query;
+import ai.grakn.graql.Query;
 import ai.grakn.graql.internal.query.QueryBuilderImpl;
 import ai.grakn.graql.macro.Macro;
 import mjson.Json;
